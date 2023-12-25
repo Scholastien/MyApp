b6d23999f507a0314074b68e9af0b43a8e078827
Scholastien Verdun <scholastien.verdun@repply.fr>
2023-12-26 00:54:54 +0100

    feat(entities): Ajout de la classe payment

---
+++ src/Domain/Entities/Payment.cs
@@ -0,0 +1,9 @@
+﻿using MyApp.Domain.Enums;
+
+namespace MyApp.Domain.Entities;
+
+public class Payment
+{
+    public PaymentTypeEnum PaymentType { get; set; }
+    public Customer Customer { get; set; }
+}
---
+++ src/Domain/Enums/PaymentTypeEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace MyApp.Domain.Enums;
+
+public enum PaymentTypeEnum
+{
+    CB = 1,
+    SEPA = 2,
+}
